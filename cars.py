5f91234cf4996ef28c1471a8b575afee81a36d5e
Keegan Greenman <keegangreenman13@gmail.com>
2023-11-27 19:05:22 -0700

    Sort - Descending

--- cars.py
+++ cars.py
@@ -25,9 +25,9 @@ cars.sort()
 # `problem 13. print the array to the console`
 print (cars)
 # `problem 14. Sort the array in descending order`
-
+cars.sort(reverse = True)
 # `problem 15. print the array to the console`
-
+print (cars)
 # `problem 16. create a variable called my_array_length with a value of the cars array length (spelling, capitilization, and spaces matter)`
 
 # `problem 17. create a variable called array_string with a value of 'The length of my array is ' (spelling, capitilization, and spaces matter)`
